f01a69ea76146e5eb14c36d96a3af9e01d9d8823
kschneider <showdialog@web.de>
2021-04-07 22:33:51 +0200

    feat quiz app

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/quiz.dart';
+
+import 'result.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'White', 'score': 3},
+        {'text': 'Orange', 'score': 0}
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Dog', 'score': 10},
+        {'text': 'Cat', 'score': 7},
+        {'text': 'Fish', 'score': 3}
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite sweet?',
+      'answers': [
+        {'text': 'Twix', 'score': 3},
+        {'text': 'Snickers', 'score': 10},
+        {'text': 'Mars', 'score': 7},
+        {'text': 'Bounty', 'score': 3}
+      ],
+    }
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    if (_questionIndex < _questionIndex + 1) {
+      setState(() {
+        _questionIndex = _questionIndex + 1;
+      });
+    }
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first Flutter App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
